80bbd63ff4af236a3b69e51162fc4a88b2a57e7b
vaishnavivasu <30413334+vaishnavivasu@users.noreply.github.com>
2017-08-07 08:34:41 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.send('Article One file is inserted here');
+    res.send("Article One file is inserted here");
 });
 
 app.get('/article-two',function(req, res){
