f1bfd7b1cb9cdd11069889da535be3a92a8850fa
vaishnavivasu <30413334+vaishnavivasu@users.noreply.github.com>
2017-08-09 10:09:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 console.log('Loaded!');
 
-//Change the text in main dev
-var element = document.getElementById('main-text');
+//Change the text of main-text div
 
-element.InnerHTML = ('Hi. This is vaish and i am super cool B)');
+var element = document.getElementById('main-text'
+);
+
+element.InnerHTML = 'Hi. This is vaish and i am super cool B)';
