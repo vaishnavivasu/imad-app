5b519bd1e33aee745b918a0521dc0b85b11103ed
vaishnavivasu <30413334+vaishnavivasu@users.noreply.github.com>
2017-08-10 11:47:26 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 
-element.InnerHTML = "New text."
+element.InnerHTML = "New text.";
+
+var img = document.getElementById('id');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+};
