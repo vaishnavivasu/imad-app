5d464a61fba936e5a4608d0060768b569554bb7c
vaishnavivasu <30413334+vaishnavivasu@users.noreply.github.com>
2017-08-10 21:33:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,15 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-
-element.InnerHTML = "New text.";
-
-var img = document.getElementById('id');
-var marginLeft = 0;
-function marginRight() {
-    marginLeft = marginLeft + 100;
-    img.style.marginLeft = marginLeft + 'px';
+var button = document.getElementById('counter');
+button.onclick = function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var counter = request.respondText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http://vaishnaviv1698.imad.hasura-app.io/counter', 'true');
+    request.send(null);
 }
-img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
-};
